175716e7a542532d523ddbe37b74bc450d699704
Youssifou Awanou <youssifou.awanou@gmail.com>
2024-09-13 18:43:02 +0000

    learn: End of TextInput

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
 import React, {useState} from 'react'
-import {View, Text, Button, StyleSheet} from "react-native";
+import {View, Text, Button, StyleSheet, TextInput} from "react-native";
 
 export default function App() {
-    const [name, setName] = useState('Stan');
-    const handlePress = () => {
-        setName(name === 'John' ? 'Steve' : 'John')
-    }
+    const [name, setName] = useState('');
+    const [age, setAge] = useState(0);
     return (
         <View style={styles.wrapper}>
             <Text style={styles.textTwo}>{name}</Text>
-            <Button
-            title={"Cliquer ici"}
-            color={'red'}
-            disabled={false}
-            onPress={handlePress}
+            <TextInput
+                value={name}
+                style={styles.input}
+                placeholder={'Votre Nom'}
+                onChangeText={(text) => setName(text)}
+            />
+            <Text style={styles.textTwo}>{age}</Text>
+            <TextInput
+                keyboardType={'numeric'}
+                style={styles.input}
+                placeholder={'Votre age'}
+                onChangeText={(text) => setAge(text)}
             />
         </View>
     )
@@ -25,7 +30,14 @@ const styles = StyleSheet.create({
         flexDirection: 'column'
     },
     viewOne: {backgroundColor: 'green'},
-    textOne: {fontWeight: 'bold'},
+    textOne: {fontWeight: 'bold', textAlign: 'center'},
     viewTwo: {backgroundColor: 'tomato'},
-    textTwo: {fontSize: 20, fontWeight: 'bold', color: 'black'},
+    textTwo: {fontSize: 20, fontWeight: 'bold', textAlign: 'center'},
+
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
 })
