e45135222bd965e52aaf268b039077a59300da23
Youssifou Awanou <youssifou.awanou@gmail.com>
2024-09-13 18:20:45 +0000

    learn: End of buttons

--- App.js
+++ App.js
@@ -1,26 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {View, Text, Button, StyleSheet} from "react-native";
 
-const Person = ({name, age, children}) => {
-    return (
-        <View>
-            <Text style={styles.textTwo}>
-                I'am {name} {age?` | age ${age}`: ''} | {children}
-            </Text>
-        </View>
-    );
-};
-
 export default function App() {
-    const getNames = (firstname, secondName, lastname) => {
-        return `${firstname} ${secondName} ${lastname}`
+    const [name, setName] = useState('Stan');
+    const handlePress = () => {
+        setName(name === 'John' ? 'Steve' : 'John')
     }
     return (
         <View style={styles.wrapper}>
-            <Person name={"Manitou"}> Alian</Person>
-            <Person name={"Stan"}> Human</Person>
-            <Person name={"Steve"}> Human</Person>
-            <Person name={"Roger"} age={46}> Human</Person>
+            <Text style={styles.textTwo}>{name}</Text>
+            <Button
+            title={"Cliquer ici"}
+            color={'red'}
+            disabled={false}
+            onPress={handlePress}
+            />
         </View>
     )
 }
